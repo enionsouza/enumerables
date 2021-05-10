579d621693f97ff2c6ae7db23f87280d17ecef5a
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-05-10 18:21:27 -0300

    Add all traversal tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -77,28 +77,66 @@ RSpec.describe 'Enumerable methods tests' do
     end
   end
 
-  # describe '#my_each' do
-  # end
+  describe '#my_each replicates #each' do
+    context 'when working on nonempty Enumerables' do
+      examples.each do |structure, example|
+        it "on #{structure.to_s.capitalize}" do
+          my_result = []
+          example.my_each { |val| my_result.push(val) }
+          std_result = []
+          example.each { |val| std_result.push(val) }
+          expect(my_result).to eq(std_result)
+        end
+      end
+    end
 
-  describe '#my_select' do
+    context 'when Enumerables are empty' do
+      it 'on Array' do
+        my_result = []
+        [].my_each { |val| my_result.push(val) }
+        std_result = []
+        [].each { |val| std_result.push(val) }
+        expect(my_result).to eq(std_result)
+      end
+
+      it 'on Hash' do
+        my_result = []
+        {}.my_each { |val| my_result.push(val) }
+        std_result = []
+        {}.each { |val| std_result.push(val) }
+        expect(my_result).to eq(std_result)
+      end
+    end
+  end
+
+  describe '#my_select replicates #select' do
     traversal_tests(:my_select, :select, examples, blocks)
   end
 
-  # describe '#my_inject' do
-  # end
+  describe '#my_inject replicates #inject' do
+    inject_blocks = { array: proc { |acc, val| acc + val },
+                      hash: proc { |acc, val| acc.push(val[1]) },
+                      range: proc { |acc, n| acc * n } }
+    traversal_tests(:my_inject, :inject, examples, inject_blocks)
+  end
 
-  # describe '#my_all?' do
-  # end
+  describe '#my_all? replicates #all?' do
+    traversal_tests(:my_all?, :all?, examples, blocks)
+  end
 
-  # describe '#my_any?' do
-  # end
+  describe '#my_any? replicates #any?' do
+    traversal_tests(:my_any?, :any?, examples, blocks)
+  end
 
-  # describe '#my_none?' do
-  # end
+  describe '#my_none? replicates #none?' do
+    traversal_tests(:my_none?, :none?, examples, blocks)
+  end
 
-  # describe '#my_count' do
-  # end
+  describe '#my_count replicates #count' do
+    traversal_tests(:my_count, :count, examples, blocks)
+  end
 
-  # describe '#my_map' do
-  # end
+  describe '#my_map replicates #map' do
+    traversal_tests(:my_map, :map, examples, blocks)
+  end
 end
