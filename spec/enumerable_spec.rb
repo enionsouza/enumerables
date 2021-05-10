ad2ede38174470aec4756282404d39d5207b9a26
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-05-10 18:01:50 -0300

    Abstracted out traversal tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,36 +1,60 @@
 require 'spec_helper'
 require_relative '../src/enumerable'
 
-def traversal_tests_mutable(method); end
+def traversal_empty(method, model, &block)
+  context 'when working on empty Enumerables' do
+    it 'on Array' do
+      my_result = [].send(method, &block)
+      std_result = [].send(model, &block)
+      expect(my_result).to eq(std_result)
+    end
+
+    it 'on Hash' do
+      my_result = {}.send(method, &block)
+      std_result = {}.send(model, &block)
+      expect(my_result).to eq(std_result)
+    end
+  end
+end
+
+def traversal_tests(method, model, examples, blocks)
+  context 'when working on nonempty Enumerables' do
+    let(:examples) { examples }
+    let(:blocks) { blocks }
+    examples.each do |structure, example|
+      it "on #{structure.to_s.capitalize}" do
+        my_result = example.send(method, &blocks[structure])
+        std_result = example.send(model, &blocks[structure])
+        expect(my_result).to eq(std_result)
+      end
+    end
+  end
+  traversal_empty(method, model, &blocks[:array])
+end
+
+examples = { array: [1, 2, 3, 4, 5],
+             hash: { developer: 'Kenny', fruit: 'Apple', dog: 'Serú' },
+             range: (1..10) }
+blocks = { array: proc { |n| n > 2 },
+           hash: proc { |_, val| val == 'Apple' },
+           range: proc { |n| n > 2 } }
 
 RSpec.describe 'Enumerable methods tests' do
-  let(:array) { [1, 2, 3, 4, 5] }
-  let(:hash) { { developer: 'Kenny', fruit: 'Apple', dog: 'Serú' } }
-  let(:range) { (1..10) }
+  let(:array) { examples[:array] }
+  let(:hash) { examples[:hash] }
+  let(:range) { examples[:range] }
+  let(:examples) { examples }
+
   describe '#my_each_with_index replicates #each_with_index' do
     context 'when working on nonempty Enumerables' do
-      it 'on Array' do
-        my_result = []
-        array.my_each_with_index { |val, i| my_result.push([val, i]) }
-        std_result = []
-        array.each_with_index { |val, i| std_result.push([val, i]) }
-        expect(my_result).to eq(std_result)
-      end
-
-      it 'on Hash' do
-        my_result = []
-        hash.my_each_with_index { |val, i| my_result.push([val, i]) }
-        std_result = []
-        hash.each_with_index { |val, i| std_result.push([val, i]) }
-        expect(my_result).to eq(std_result)
-      end
-
-      it 'on Range' do
-        my_result = []
-        range.my_each_with_index { |val, i| my_result.push([val, i]) }
-        std_result = []
-        range.each_with_index { |val, i| std_result.push([val, i]) }
-        expect(my_result).to eq(std_result)
+      examples.each do |structure, example|
+        it "on #{structure.to_s.capitalize}" do
+          my_result = []
+          example.my_each_with_index { |val, i| my_result.push([val, i]) }
+          std_result = []
+          example.each_with_index { |val, i| std_result.push([val, i]) }
+          expect(my_result).to eq(std_result)
+        end
       end
     end
 
@@ -56,8 +80,9 @@ RSpec.describe 'Enumerable methods tests' do
   # describe '#my_each' do
   # end
 
-  # describe '#my_select' do
-  # end
+  describe '#my_select' do
+    traversal_tests(:my_select, :select, examples, blocks)
+  end
 
   # describe '#my_inject' do
   # end
